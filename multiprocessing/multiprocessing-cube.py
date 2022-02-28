e14ee9ce006c56b3eb5ad5bd222d40d8551afe0a
Rafał Chojnacki <rafalchojnacki0502@gmail.com>
2022-02-28 15:52:28 +0100

    added comments

--- multiprocessing/multiprocessing-cube.py
+++ multiprocessing/multiprocessing-cube.py
@@ -11,11 +11,12 @@ def cube(x):
 if __name__ =="__main__":
     processes = []
     for i in range(5):
-        p = Process(target=cube, args=(i,))
+        p = Process(target=cube, args=(i,)) # target specified the function to be called
         processes.append(p)
-        p.start()
+        p.start() # start the process
     
-    for p in processes:
-        p.join()
+    for p in processes: # all processes looped over to wait until every process execution is complete, which
+        p.join()        # is detected using the join() method. It helps in making sure that the rest of the 
+                        # program runs only after the multiprocessing is complete
         
     
